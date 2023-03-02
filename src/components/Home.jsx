76f4f56b81446d40ae4b70fd98264cd958388e8c
Diego Andrés Parra Espinosa <113558619+DiegoParraE@users.noreply.github.com>
2023-03-01 21:28:53 -0500

    Agregando componentes

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Header } from './Header'
+import { Footer } from './Footer'
+import { Navegation } from './Navegation'
+
+export const Home = () => {
+  return (
+    <h1>home</h1>
+  )
+}
---
+++ src/components/Navegation.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import { Home } from './Home'
+import { AboutUs } from './AboutUs'
+
+
+export const Navegation = () => {
+  return (
+    <BrowserRouter>
+        <nav className='text-left p-3 py-4'>
+            <Link className="h3" to='/'>Home</Link>
+            <Link className="h3" to='/AboutUs'>Nosotros</Link>
+        </nav>
+        <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/AboutUs' element={<AboutUs/>} />
+        </Routes>
+        </BrowserRouter>
+    
+  )
+}
