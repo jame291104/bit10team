37e6ed9574ccc876608427d6c89783e35b509019
yessica camargo <yessicacamargo15@gmail.com>
2023-03-02 20:00:55 -0500

    se agrega footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+
 
 export const Footer = () => {
   return (
-    <section>
-      <h1>This is Footer</h1>
-    </section>
+    <div className='d-flex d-block flex-row justify-context-around py-4 bg-dark text-white'>
+      <h1>Envíanos un mensaje</h1>
+      <div className='d-flex d-block flex-row justify-context-around'> 
+      <h1> github linkedin  </h1>
+      </div>
+      <div>
+      <h2>Docente: Javier García</h2>
+      <p>BIT 2023
+        Bogotá D.C - Colombia
+      </p>
+      </div>
+      <div>
+      <p>
+        sebastianlayos23@gmail.com 
+        jame291104@gmail.com 
+        yessicacamargo15@gmail.com 
+        parradiego169@gmail.com 
+      </p>
+      </div>
+    </div>
+    
   )
 }
+
+
+
+export default Footer
