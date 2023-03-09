b7bab2d7e19279f4ef3ed8a5707220b9c1c3f16c
sbslayos <german.martinez.mt@usa.edu.co>
2023-03-08 21:40:47 -0500

    m

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import { Navegation } from './components/Navegation';
 
+
 function App() {
+ 
 
   return (
+    <>
       <Navegation/>
+    </>
   )
 }
 
