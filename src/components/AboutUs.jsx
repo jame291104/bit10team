2aceaff22a5fb256fc88ffd6a0a22bb49b11047c
Jesus Moran Espinoza <jame291104@gmail.com>
2023-03-16 19:11:01 -0500

    Finish

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { HeaderAU } from './HeaderAU'
 import { Footer } from './Footer'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export const AboutUs = () => {
   return (
@@ -18,82 +20,72 @@ export const AboutUs = () => {
       <p className='p-5 parrafoAU'>A pleasure! We are Tinto Frío, a work team made up of four frontend developers who decided to bring an idea to reality while having a coffee in the middle of a virtual meeting, while we were thinking about how to implement the development of the idea, 
       that coffee became cold, from that moment we have worked on our project drinking cold coffee while we implement it. <br/> Get to know us a little more:</p>
     
-      <div>
-    
-        <table className='d-flex flex-row justify-content-around'>
-    
-          <tr className='d-flex flex-row justify-content-around'>
-            
-            <td className='tdAU'>
-              <a href="https://yessicac98.github.io/bit07me/" 
-                 className='aAU'>
-                <img src="https://yessicac98.github.io/bit07me/docs/img/me.jpeg" 
-                     alt="Desarrolladora Yessica" 
-                     class='imgAU'
-                     width={130}
-                     height={180}
-                     />
-                <p className='psAU'>
-                  Yessica Camargo
-                </p>
-               </a>
-            </td>
-          </tr>  
-
-          <tr>    
-            <td className='tdAU'>
-              <a href="https://jame291104.github.io/bit07me/" 
-                 className='aAU'>
-                <img src="https://jame291104.github.io/bit07me/img/me.jpg" 
-                     alt="Desarrollador Jesús" 
-                     class='imgAU'
-                     width={130}
-                     height={180} 
-                     />
-                <p className='psAU'>
-                  Jesús Morán
-                </p>
-              </a>
-            </td>
-          </tr>
-
-          <tr>
-            <td className='tdAU'>
-              <a href="https://sbslayos.github.io/bit07me/" 
-                 className='aAU'>
-                <img src="https://sbslayos.github.io/bit07me/img/me.jpeg" 
-                     alt="Desarrollador Sebastián" 
-                     class='imgAU'
-                     width={120}
-                     height={180} 
-                     />
-                <p className='psAU'>
-                  Sebastian Layos 
-                </p>
-              </a>
-            </td>
-          </tr>
-
-          <tr>
-            <td className='tdAU'>
-              <a href="https://diegoparrae.github.io/bit07me/"
-                 className='aAU'> 
-                <img src="https://diegoparrae.github.io/bit07me/img/me.jpeg" 
-                     alt="Desarrollador Diego" 
-                     class='imgAU'
-                     width={130}
-                     height={180} 
-                     />
-                <p className='psAU'>
-                
-                  Diego Parra
-                </p>
-              </a>
-            </td>
-          </tr>
-
-        </table>
-
+      <div className='my-5 row justify-content-center  mx-0'>
+      <Card className='m-4' style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="https://yessicac98.github.io/bit07me/docs/img/me.jpeg"
+        width={286}
+        height={370}
+        />
+          <Card.Body>
+            <Card.Title>Yessica Camargo</Card.Title>
+              <Card.Text>
+              Fullstack Web Developer
+              <br />
+              - In charge of project documentation and its process in Trello
+              </Card.Text>
+              <a href="https://yessicac98.github.io/bit07me/" target={'_blank'}><Button variant="primary">See more</Button></a>
+              <a href="https://github.com/YessicaC98" target={'_blank'}><Button className='mx-2' variant="dark">GitHub <i className="bi bi-github"></i> </Button></a>
+              
+          </Card.Body>
+      </Card>
+      <Card className='m-4' style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="https://jame291104.github.io/bit07me/img/me.jpg"
+        width={286}
+        height={370}
+        />
+          <Card.Body>
+            <Card.Title>Jesús Morán Espinoza</Card.Title>
+              <Card.Text>
+              Front-End Web Developer React Js.
+              <br />
+              - Team leader, in charge of deployment and version control in Git and Github 
+              </Card.Text>
+              <a href="https://jame291104.github.io/bit07me/" target={'_blank'}><Button variant="primary">See more</Button></a>
+              <a href="https://github.com/jame291104" target={'_blank'}><Button className='mx-2' variant="dark">GitHub <i className="bi bi-github"></i> </Button></a>
+          </Card.Body>
+      </Card>
+      <Card className='m-4' style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="https://sbslayos.github.io/bit07me/img/me.jpeg"
+        width={286}
+        height={370}
+        />
+          <Card.Body>
+            <Card.Title>Sebastián Layos</Card.Title>
+              <Card.Text>
+              Fullstack Web Developer.
+              <br />
+              - Favorites section creating and rendering.
+              </Card.Text>
+              <a href="https://sbslayos.github.io/bit07me/" target={'_blank'}><Button variant="primary">See more</Button></a>
+              <a href="" target={'_blank'}><Button className='mx-2' variant="dark">GitHub <i className="bi bi-github"></i> </Button></a>
+          </Card.Body>
+      </Card>
+      <Card className='m-4' style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="https://diegoparrae.github.io/bit07me/img/me.jpeg"
+        width={286}
+        height={370}
+        />
+          <Card.Body>
+            <Card.Title>Diego Parra</Card.Title>
+              <Card.Text>
+                Front-End Web Developer React Js.
+                <br />
+                - UX/UI Design and CSS's styles of Library Gamer.
+              </Card.Text>
+              <a href="https://diegoparrae.github.io/bit07me/" target={'_blank'}><Button variant="primary">See more</Button></a>
+              <a href="https://github.com/DiegoParraE" target={'_blank'}><Button className='mx-2' variant="dark">GitHub <i className="bi bi-github"></i> </Button></a>
+          </Card.Body>
+      </Card>
       </div>
 
     </div>
